98cdb7d6489d1172a7f1739e32d340b6ac8d9d35
Dimitrios Akritidis <Jimko25@yahoo.gr>
2021-04-29 16:49:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const apiUrl = 'http://localhost:5000/'
+
+  axios.get('http://localhost:5000/movies')
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello guys </h1>
+      
     </div>
   );
 }
