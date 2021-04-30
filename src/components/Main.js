89c1a0b4b2a20e204d4a2672de63e90f016ce7b8
hangingpulse <76062791+hangingpulse@users.noreply.github.com>
2021-04-30 18:15:04 +0200

    connect to backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "./Main.css";
 
 function Main({ movies }) {
+    const [movieInput, setMovieInput] = useState({
+        title: "",
+        director: "",
+        screenwriter: "",
+        moviecast: "",
+        runtime: "",
+        year: "",
+        rating: "",
+    });
+
+    const addMovie = () => {
+        axios
+            .post("http://localhost:5000/movies", movieInput)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
     return (
         <div className="Main">
+            {/* input field --> state --> button --> post request */}
+
+            <form className="form">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="add a title"
+                    onChange={(e) =>
+                        setMovieInput({ ...movieInput, title: e.target.value })
+                    }
+                    value={movieInput.title}
+                />
+                <input
+                    type="text"
+                    name="director"
+                    placeholder="add a director"
+                    onChange={(e) =>
+                        setMovieInput({
+                            ...movieInput,
+                            director: e.target.value,
+                        })
+                    }
+                    value={movieInput.director}
+                />
+                <input
+                    type="text"
+                    name="screenwriter"
+                    placeholder="add a screenwriter"
+                    onChange={(e) =>
+                        setMovieInput({
+                            ...movieInput,
+                            screenwriter: e.target.value,
+                        })
+                    }
+                    value={movieInput.screenwriter}
+                />
+                <input
+                    type="text"
+                    name="Cast"
+                    placeholder="add the cast"
+                    onChange={(e) =>
+                        setMovieInput({
+                            ...movieInput,
+                            moviecast: e.target.value,
+                        })
+                    }
+                    value={movieInput.moviecast}
+                />
+                <input
+                    type="number"
+                    name="year"
+                    placeholder="add a year"
+                    onChange={(e) =>
+                        setMovieInput({ ...movieInput, year: e.target.value })
+                    }
+                    value={movieInput.year}
+                />
+                <input
+                    type="number"
+                    name="runtime"
+                    placeholder="add the runtime"
+                    onChange={(e) =>
+                        setMovieInput({
+                            ...movieInput,
+                            runtime: e.target.value,
+                        })
+                    }
+                    value={movieInput.runtime}
+                />
+                <input
+                    type="number"
+                    name="rating"
+                    placeholder="add the rating"
+                    onChange={(e) =>
+                        setMovieInput({
+                            ...movieInput,
+                            rating: e.target.value,
+                        })
+                    }
+                    value={movieInput.rating}
+                />
+                <input
+                    className="add-button"
+                    type="submit"
+                    value="Submit the movie"
+                    onClick={addMovie}
+                />
+            </form>
+
             {movies.map((item) => (
                 <>
-                    <div>{item.title}</div>
-                    <div>{item.director}</div>
-                    <div>{item.screenwriter}</div>
-                    <div>{item.moviecast}</div>
-                    <div>{item.runtime}</div>
-                    <div>{item.year}</div>
-                    <div>{item.rating}</div>
+                    <div>Title: {item.title}</div>
+                    <div>Director: {item.director}</div>
+                    <div>Screenwriter: {item.screenwriter}</div>
+                    <div>Cast: {item.moviecast}</div>
+                    <div>Runtime: {item.runtime}min</div>
+                    <div>Release Date: {item.year}</div>
+                    <div>imdb Rating: {item.rating}</div>
                 </>
             ))}
         </div>
